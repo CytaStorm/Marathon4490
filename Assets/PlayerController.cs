e77b9ab1effe5fba82386800c0e93eead67431b4
Ziad-J05 <143660614+Ziad-J05@users.noreply.github.com>
2025-02-16 18:15:46 -0500

    started working on jumps

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -7,31 +7,36 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] float speed;
-    [SerializeField] float jumpStrength;
-    [SerializeField] float gravity;
-    [SerializeField] float terminalVelocity;
+    [SerializeField] float _speed;
+    [SerializeField] float _jumpStrength;
+    [SerializeField] float _gravity;
+    [SerializeField] float _terminalVelocity;
 
     [SerializeField] float _buttonAccel;
     [SerializeField] float _xDrag;
 
-    private Rigidbody rigidBody;
-    private Vector3 direction;
+    private Rigidbody _rigidBody;
+    private Vector3 _direction;
+
+    [SerializeField] float _maxJumpTime = 1f;
+    private float _jumpTimer = 0f;
+    private bool _jumpInputOn = false;
+    private bool _onGround = true;
 
     // Start is called before the first frame update
     void Start()
     {
-        rigidBody = gameObject.GetComponent<Rigidbody>();
-        rigidBody.freezeRotation = true;
+        _rigidBody = gameObject.GetComponent<Rigidbody>();
+        _rigidBody.freezeRotation = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        float acceleration = direction.x * 10;
-        print(acceleration);
+        // HORIZONTAL MOTION
+        float acceleration = _direction.x * 10;
 
-        Vector3 velocity = rigidBody.velocity;
+        Vector3 velocity = _rigidBody.velocity;
         velocity.x += (acceleration * Time.deltaTime);
 
         if (velocity.x > 0)
@@ -43,7 +48,25 @@ public class PlayerController : MonoBehaviour
         {
             velocity.x = Mathf.Clamp(velocity.x + (_xDrag * Time.deltaTime), float.MinValue, 0);
         }
-        rigidBody.velocity = velocity;
+
+        // VERTICAL MOTION
+        if (_jumpInputOn)
+        {
+            // Apply velocity
+            velocity.y += _jumpStrength * Time.deltaTime;
+
+            // Jumptimer calculations
+            _jumpTimer += Time.deltaTime;
+            if (_jumpTimer > _maxJumpTime)
+            {
+                _jumpTimer = 0f;
+                _jumpInputOn = false;
+            }
+        }
+
+        // APPLY MOTION
+        _rigidBody.velocity = velocity;
+
     }
 
     public void Move(InputAction.CallbackContext Move)
@@ -51,27 +74,49 @@ public class PlayerController : MonoBehaviour
         if (Move.performed)
         {
             float input = Move.ReadValue<float>();
-            print(input);
+            //print(input);
 
-            direction = new Vector3(input, 0, 0);
+            _direction = new Vector3(input, 0, 0);
         }
         else if (Move.canceled)
         {
-            direction = Vector3.zero;
+            _direction = Vector3.zero;
         }
                 
     }
 
     public void Jump(InputAction.CallbackContext Jump)
     {
-        if (Jump.performed)
+        if ((Jump.performed) && (_onGround))
         {
             Debug.Log("JUMP");
+
+            _jumpInputOn = true;
+
+            /*
             Vector3 velocity = rigidBody.velocity;
             velocity.y = jumpStrength;
             rigidBody.velocity = velocity;
+            */
+        }
+        else if (Jump.canceled)
+        {
+            _jumpInputOn = false;
         }
     }
 
-    
+    private void OnCollisionEnter(Collision collision)
+    {
+        //TODO: check if collision is ground
+        _onGround = true;
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        //TODO: check if collision is ground
+        _onGround = false;
+    }
+
+
+
 }
