92bac252d90a14e2cb40af734a70998963223476
Jeff Chen (RIT Student) <jc7117@rit.edu>
2025-02-21 20:20:25 -0500

    jump feels pretty alright

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -7,126 +7,133 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] float _speed;
-    [SerializeField] float _initialJump;
-    [SerializeField] float _jumpStrength;
-    [SerializeField] float _gravity;
-    [SerializeField] float _terminalVelocity;
+	[SerializeField] float _gravity;
+	private float _appliedGravity;
 
-    [SerializeField] float _buttonAccel;
-    [SerializeField] float _xDrag;
+	[SerializeField] private AnimationCurve _gravityCurve;
 
-    private Rigidbody _rigidBody;
-    private Vector3 _direction;
+	[SerializeField] float _xDrag;
 
-    [SerializeField] float _maxJumpTime = 1f;
-    private float _jumpTimer = 0f;
-    private bool _jumpInputOn = false;
-    private bool _onGround = true;
+	private Rigidbody _rigidBody;
+	private Vector3 _direction;
+
+	private bool _onGround = false;
+
+
+    [SerializeField] private float jumpShortSpeed = 3f;   // Velocity for the lowest jump
+    [SerializeField] private float jumpSpeed = 6f;          // Velocity for the highest jump
+
+	bool jump = false;
+	bool jumpCancel = false;
 
     // Start is called before the first frame update
     void Start()
+	{
+		_rigidBody = gameObject.GetComponent<Rigidbody>();
+		_rigidBody.freezeRotation = true;
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+		if (Input.GetButtonDown("Jump") && _onGround)
+		{
+			// Player starts pressing the button
+			jump = true;
+		}
+		if (Input.GetButtonUp("Jump") && _onGround)
+		{     // Player stops pressing the button
+			jumpCancel = true;
+		}
+
+		// HORIZONTAL MOTION
+		float acceleration = _direction.x * 10;
+
+		Vector3 velocity = _rigidBody.velocity;
+		velocity.x += (acceleration * Time.deltaTime);
+
+		if (velocity.x > 0)
+		{
+			//apply xdrag
+			velocity.x = Mathf.Clamp(velocity.x - (_xDrag * Time.deltaTime), 0, float.MaxValue);
+		}
+		else if (velocity.x < 0)
+		{
+			velocity.x = Mathf.Clamp(velocity.x + (_xDrag * Time.deltaTime), float.MinValue, 0);
+		}
+
+		// APPLY MOTION
+		_rigidBody.velocity = velocity;
+	}
+
+	void FixedUpdate()
     {
-        _rigidBody = gameObject.GetComponent<Rigidbody>();
-        _rigidBody.freezeRotation = true;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-        if (_jumpTimer < _maxJumpTime && !_onGround)
+        // Normal jump (full speed)
+        if (jump)
         {
-            //apply gravity along curve
-            _gravity = Mathf.Lerp(-9.8f, 9.8f, _jumpTimer/_maxJumpTime);
-            _jumpTimer = Mathf.Clamp(_jumpTimer + Time.deltaTime, 0, _maxJumpTime);
+            _rigidBody.velocity = new Vector3(_rigidBody.velocity.x, jumpSpeed, 0);
+            jump = false;
         }
-        else
+        // Cancel the jump when the button is no longer pressed
+        if (jumpCancel)
         {
-            _gravity = 9.8f;
-        }
-        //apply gravity
-        _rigidBody.AddForce(Vector3.down * _gravity, ForceMode.Acceleration);
-        
-
-        // HORIZONTAL MOTION
-        float acceleration = _direction.x * 10;
-
-        Vector3 velocity = _rigidBody.velocity;
-        velocity.x += (acceleration * Time.deltaTime);
-
-        if (velocity.x > 0)
-        {
-            //apply xdrag
-            velocity.x = Mathf.Clamp(velocity.x - (_xDrag * Time.deltaTime), 0, float.MaxValue);
-        }
-        else if (velocity.x < 0)
-        {
-            velocity.x = Mathf.Clamp(velocity.x + (_xDrag * Time.deltaTime), float.MinValue, 0);
-        }
-
-        // APPLY MOTION
-        _rigidBody.velocity = velocity;
-        
-        print(_jumpTimer);
-    }
-
-    public void Move(InputAction.CallbackContext Move)
-    {
-        if (Move.performed)
-        {
-            float input = Move.ReadValue<float>();
-            //print(input);
-
-            _direction = new Vector3(input, 0, 0);
-        }
-        else if (Move.canceled)
-        {
-            _direction = Vector3.zero;
-        }
-                
-    }
-
-    private void EndJump()
-    {
-        _jumpInputOn = false;
-        _jumpTimer = 0f;
-        _rigidBody.AddForce(new Vector3(0, -160, 0), ForceMode.Acceleration);
-    }
-
-    public void Jump(InputAction.CallbackContext Jump)
-    {
-        if ((Jump.performed) && (_onGround))
-        {
-            Debug.Log("JUMP");
-            //Vector3 velocity = _rigidBody.velocity;
-            //velocity.y = _initialJump;
-            //_rigidBody.velocity = velocity;
-            _onGround = false;
-            //set gravity lower
-        }
-        else if (Jump.canceled)
-        {
-            Debug.Log("JUMP off");
-            //Vector3 velocity = _rigidBody.velocity;
-            //velocity.y /= 2;
-            //_rigidBody.velocity = velocity;
+            if (_rigidBody.velocity.y > jumpShortSpeed)
+                _rigidBody.velocity = new Vector2(_rigidBody.velocity.x, jumpShortSpeed);
+            jumpCancel = false;
         }
     }
 
-    private void OnCollisionEnter(Collision collision)
-    {
-        //TODO: check if collision is ground
-        _onGround = true;
-        print("ON Ground");
-    }
+	public void Move(InputAction.CallbackContext Move)
+	{
+		print("here");
+		if (Move.performed)
+		{
+			float input = Move.ReadValue<float>();
 
-    private void OnCollisionExit(Collision collision)
-    {
-        //TODO: check if collision is ground
-        _onGround = false;
-        print("OFF Ground");
-    }
+			_direction = new Vector3(input, 0, 0);
+		}
+		else if (Move.canceled)
+		{
+			_direction = Vector3.zero;
+		}
+				
+	}
+	public void Jump(InputAction.CallbackContext Jump)
+	{
+		if ((Jump.performed) && (_onGround))
+		{
+			Debug.Log("JUMP");
+			//Vector3 velocity = _rigidBody.velocity;
+			//velocity.y = _initialJump;
+			//_rigidBody.velocity = velocity;
+			_onGround = false;
+			jump = true;
+			//set gravity lower
+		}
+		else if (Jump.canceled)
+		{
+			Debug.Log("JUMP off");
+			jumpCancel = true;
+			//Vector3 velocity = _rigidBody.velocity;
+			//velocity.y /= 2;
+			//_rigidBody.velocity = velocity;
+		}
+	}
+
+	private void OnCollisionEnter(Collision collision)
+	{
+		//TODO: check if collision is ground
+		_onGround = true;
+		print("ON Ground");
+	}
+
+	private void OnCollisionExit(Collision collision)
+	{
+		//TODO: check if collision is ground
+		_onGround = false;
+		print("OFF Ground");
+	}
 
 
 
